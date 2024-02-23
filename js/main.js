376f56d5e697ace2bc6103786bdd912129709ae4
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-23 11:23:19 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 // Array Visualizer Start
 
 // Global Variables
-let data = [1, 2, 3, 5, 8];
+let data = [1, 2, 3, 5, 8, 7, 4, 4, 1, 0, 8, 9, 10, 7];
 let maxVal = 10; // max data value
+
+// HTML Variables
+let outputEl = document.getElementById("container");
+
+// Tranverse to Display Data Arrray
+function drawDataArray() {
+  let outputStr = " ";
+  for (let i = 0; i < data.length; i++) {
+    outputStr += `<div>${data[i]}</div>`;
+  }
+  for (let val of data) {
+    outputStr += `<div>${val[i]}</div>`;
+  }
+  outputEl.innerHTML = outputStr;
+}
+
+drawDataArray();
